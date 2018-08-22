c42d3562730de5981785ff7b2b9947954fd2c6f0
c3rberuss <c3rberuss@gmail.com>
2018-08-21 22:17:07 -0600

    template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent }
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+      RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
